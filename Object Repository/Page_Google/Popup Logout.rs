3cd2bc171a4515b50ed7239978abb9b50df8fb5b
idx_prj_web <suhardi@indocyber.id>
2024-07-30 15:24:41 +0700

    12121

---
+++ Object Repository/Page_Google/Popup Logout.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Popup Logout</name>
+   <tag></tag>
+   <elementGuidId>d892ef33-83ad-4163-ba99-6256de7545ba</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div/c-wiz/div/div/c-wiz/div/div/div/div[2]/div[2]/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
